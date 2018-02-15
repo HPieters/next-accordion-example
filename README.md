c3d39daccf101be880b5300bc8d1a37059ca5880
Harrie Pieters <harriepieters@gmail.com>
2018-02-15 18:11:06 +0100

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Next-accordion-example
+
+Quick example using Next.js and react-accessible-accordion to show warning.
---
+++ pages/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+    Accordion,
+    AccordionItem,
+    AccordionItemTitle,
+    AccordionItemBody,
+} from 'react-accessible-accordion';
+
+import '../style.css';
+
+const Example = () => (
+    <Accordion>
+        <AccordionItem>
+            <AccordionItemTitle>
+                <h3>Simple title</h3>
+            </AccordionItemTitle>
+            <AccordionItemBody>
+                <p>
+                    Body content
+                </p>
+            </AccordionItemBody>
+        </AccordionItem>
+        <AccordionItem>
+            <AccordionItemTitle>
+                <h3>Complex title</h3>
+                <div>With a bit of description</div>
+            </AccordionItemTitle>
+            <AccordionItemBody>
+                <p>
+                    Body content
+                </p>
+            </AccordionItemBody>
+        </AccordionItem>
+    </Accordion>
+);
+
+export default Example;
