cb9ab4c92631f7d2784bddfee1090cd44d27ac49
Harrie Pieters <harriepieters@gmail.com>
2018-03-18 12:48:21 +0100

    Added isomorphic fix resetNextUuid

--- pages/index.js
+++ pages/index.js
@@ -4,34 +4,40 @@ import {
     AccordionItem,
     AccordionItemTitle,
     AccordionItemBody,
+    resetNextUuid,
 } from 'react-accessible-accordion';
 
 import '../style.css';
 
-const Example = () => (
-    <Accordion>
-        <AccordionItem>
-            <AccordionItemTitle>
-                <h3>Simple title</h3>
-            </AccordionItemTitle>
-            <AccordionItemBody>
-                <p>
-                    Body content
-                </p>
-            </AccordionItemBody>
-        </AccordionItem>
-        <AccordionItem>
-            <AccordionItemTitle>
-                <h3>Complex title</h3>
-                <div>With a bit of description</div>
-            </AccordionItemTitle>
-            <AccordionItemBody>
-                <p>
-                    Body content
-                </p>
-            </AccordionItemBody>
-        </AccordionItem>
-    </Accordion>
-);
+const Example = () => {
+    // Reset uuid
+    resetNextUuid();
+
+    return (
+        <Accordion>
+            <AccordionItem>
+                <AccordionItemTitle>
+                    <h3>Simple title</h3>
+                </AccordionItemTitle>
+                <AccordionItemBody>
+                    <p>
+                        Body content
+                    </p>
+                </AccordionItemBody>
+            </AccordionItem>
+            <AccordionItem>
+                <AccordionItemTitle>
+                    <h3>Complex title</h3>
+                    <div>With a bit of description</div>
+                </AccordionItemTitle>
+                <AccordionItemBody>
+                    <p>
+                        Body content
+                    </p>
+                </AccordionItemBody>
+            </AccordionItem>
+        </Accordion>
+    );
+};
 
 export default Example;
